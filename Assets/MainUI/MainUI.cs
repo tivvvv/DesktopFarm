5035efd82bf97eb274b91ac0828e20727a8a58db
tivvvv <18612553935@163.com>
2025-04-25 13:18:14 +0800

    feat: 购买动物.

--- Assets/MainUI/MainUI.cs
+++ Assets/MainUI/MainUI.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class MainUI : MonoBehaviour
 {
-    public int money = 0;
+    public int money = 100;
 
     public TextMeshProUGUI moneyText;
 
@@ -13,9 +13,53 @@ public class MainUI : MonoBehaviour
 
     public int chickenNum = 0;
 
+    public Transform Animals;
+
+    public GameObject StoreUI;
+
+    public GameObject chick;
+
+    public GameObject chicken;
+
     private void Update()
     {
         moneyText.text = money.ToString();
         animalText.text = (chickNum + chickenNum).ToString();
+    }
+
+    public void CloseStore()
+    {
+        StoreUI.SetActive(false);
+    }
+
+    public void OpenStore()
+    {
+        StoreUI.SetActive(true);
+    }
+
+    public void BuyChick()
+    {
+        if (money < 50)
+        {
+            return;
+        }
+
+        money -= 50;
+        GameObject chickObj = Instantiate(chick, Animals);
+        float x = Random.Range(-2.5F, 0.7F);
+        chickObj.transform.localPosition = new Vector3(x, 2, 0);
+        chickNum++;
+    }
+    public void BuyChicken()
+    {
+        if (money < 100)
+        {
+            return;
+        }
+        money -= 100;
+        GameObject chickenObj = Instantiate(chicken, Animals);
+        float x = Random.Range(-2.5F, 0.7F);
+        chickenObj.transform.localPosition = new Vector3(x, 2, 0);
+        chickenNum++;
     }
 }
